4993500ceedfc927bdd86f510e68fd3274d35677
xbmchina <1131663064@qq.com>
2020-06-11 23:07:14 +0800

    一个项目多个mq源

---
+++ rabbitmq-demo/multi-rabbit-demo/src/main/java/cn/xbmchina/multirabbitdemo/MultiRabbitDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.xbmchina.multirabbitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiRabbitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiRabbitDemoApplication.class, args);
+    }
+
+}
